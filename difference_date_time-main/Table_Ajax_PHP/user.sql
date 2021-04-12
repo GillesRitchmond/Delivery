960d8037ebddb0df68e3a2bb3ccf32d10b54738c
GillesRitchmond <gritchmond@gmail.com>
2021-04-12 04:32:27 -0400

    SQL Query to import tbl_user in your testing database

--- difference_date_time-main/Table_Ajax_PHP/user.sql
+++ difference_date_time-main/Table_Ajax_PHP/user.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2021 at 06:56 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Apr 11, 2021 at 09:33 PM
+-- Server version: 10.4.17-MariaDB
+-- PHP Version: 7.2.34
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,33 +24,40 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `user`
+-- Table structure for table `tbl_user`
 --
 
-CREATE TABLE `user` (
+CREATE TABLE `tbl_user` (
   `id` int(11) NOT NULL,
-  `first_name` varchar(200) NOT NULL,
-  `last_name` varchar(200) NOT NULL
+  `numero_fiche` int(11) NOT NULL,
+  `envoyeur` varchar(250) NOT NULL,
+  `heure_envoie` datetime NOT NULL,
+  `receveur` varchar(250) NOT NULL,
+  `heure_reception` datetime NOT NULL,
+  `temps_attente` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
--- Dumping data for table `user`
+-- Dumping data for table `tbl_user`
 --
 
-INSERT INTO `user` (`id`, `first_name`, `last_name`) VALUES
-(1, 'John', 'Smith'),
-(5, 'Peterson', 'Parker'),
-(7, 'Rock', 'Madison'),
-(8, 'Titan', 'Edge');
+INSERT INTO `tbl_user` (`id`, `numero_fiche`, `envoyeur`, `heure_envoie`, `receveur`, `heure_reception`, `temps_attente`) VALUES
+(3, 2, 'Carol', '2021-04-08 03:52:37', 'Ferrari', '2021-04-08 06:52:37', '00:00:00'),
+(10, 3, 'Kenneth', '2021-04-08 10:36:06', 'Simons', '2021-04-08 21:52:06', '00:00:00'),
+(11, 4, 'Dona', '2021-04-08 04:21:38', 'Huber', '2021-04-08 08:03:38', '00:00:00'),
+(12, 5, 'Williamr', '2021-04-08 07:41:02', 'Soliz', '2021-04-08 09:33:02', '00:00:00'),
+(14, 6, 'Sarah bercy', '2021-04-08 08:50:30', 'Anne', '2021-04-08 09:40:00', '00:00:00'),
+(17, 7, 'Stéphanie', '2021-03-31 00:00:00', 'Ritchmond', '2021-04-04 00:00:00', '00:00:00'),
+(19, 8, 'Marcus', '2021-04-12 00:00:00', 'Elisabeth', '2021-04-12 00:00:00', '00:00:00');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `user`
+-- Indexes for table `tbl_user`
 --
-ALTER TABLE `user`
+ALTER TABLE `tbl_user`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -58,10 +65,10 @@ ALTER TABLE `user`
 --
 
 --
--- AUTO_INCREMENT for table `user`
+-- AUTO_INCREMENT for table `tbl_user`
 --
-ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+ALTER TABLE `tbl_user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- difference_date_time-main/table_ajax_MSQL/delete.php
+++
@@ -1,8 +0,0 @@
-<?php  
- $connect = mysqli_connect("localhost", "root", "", "testing");  
- $sql = "DELETE FROM user WHERE id = '".$_POST["id"]."'";  
- if(mysqli_query($connect, $sql))  
- {  
-      echo 'Data Deleted';  
- }  
- ?>
